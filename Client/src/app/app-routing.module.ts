53a064c2a7fac8ed20e80ca8ce28b0b0fc470532
KryniuPL <krzysztof.draganfm@gmail.com>
2018-08-06 21:51:52 +0200

    Added settings panel

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent}
+  { path: '', component: HomePageComponent },
+  { path: 'footer', component: FooterComponent},
 ];
 
 @NgModule({

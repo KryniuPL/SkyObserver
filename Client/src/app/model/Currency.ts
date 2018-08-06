0d95cef6b61f2a086256651da480bf675fa1c5d7
KryniuPL <krzysztof.draganfm@gmail.com>
2018-08-06 23:00:45 +0200

    Added two model classes

---
+++ Client/src/app/model/Currency.ts
@@ -0,0 +1,10 @@
+export class Currency {
+    name: string;
+    pathToIconFile: string;
+
+    constructor(name:string, path: string){
+        this.name = name;
+        this.pathToIconFile = path;
+    }
+    
+}

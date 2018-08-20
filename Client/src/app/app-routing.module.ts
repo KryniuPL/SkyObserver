fef106caa3051d21b98b664ebace60a26d8d3e7a
KryniuPL <krzysztof.draganfm@gmail.com>
2018-08-20 21:18:37 +0200

    Exchange button

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import { PageNotFoundComponent } from "./pages/page-not-found/page-not-found.com
 
 const routes: Routes = [
   { path: "", component: HomePageComponent },
-  { path: "footer", component: FooterComponent },
   {
     path: "not-found",
     component: PageNotFoundComponent

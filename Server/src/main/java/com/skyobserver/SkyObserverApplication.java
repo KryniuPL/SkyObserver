95e5297ce9735729bc3a5b99c4281260d6e9be1d
KryniuPL <krzysztof.draganfm@gmail.com>
2018-09-16 21:52:20 +0200

    Server application initialized

---
+++ Server/src/main/java/com/skyobserver/SkyObserverApplication.java
@@ -0,0 +1,12 @@
+package com.skyobserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyObserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyObserverApplication.class, args);
+    }
+}

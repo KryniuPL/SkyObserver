3d474fd32c83f6d19265217106a9b0ea42134f76
KryniuPL <krzysztof.draganfm@gmail.com>
2018-09-22 15:15:25 +0200

    Airports.csv and sql script added

---
+++ Server/src/main/resources/sql/filter_airports.sql
@@ -0,0 +1,4 @@
+delete from airports where airports.type = 'closed';
+delete from airports where airports.type = 'small_airport';
+delete from airports where airports.type = 'heliport';
+delete from airports where airports.type = 'seaplane_base';
